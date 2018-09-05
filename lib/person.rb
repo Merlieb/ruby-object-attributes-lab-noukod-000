69fa1a3ee89f68721b1d434dfb139bc35beb9a8a
Merline Belizaire <merline@noukod.com>
2018-09-05 19:58:54 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Dog
+    def name=(name)
+      @name = name
+    end
+
+    def name
+    @name
+    end
+
+    def breed
+       @breed
+    end
+
+    def malou(malou)
+      @malou = malou
+    end
+
+    def malou
+    @malou
+    end
+  end
